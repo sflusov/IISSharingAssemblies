15421ef3d518d1acef838de247ad0737843e74d7
Stanislav Flusov <Stanislav.Flusov@mysale.com>
2017-09-19 23:41:44 +0300

    common-assemblies and script for dev machine

---
+++ powershell-scripts/install-assebmlies-to-gac.ps1
@@ -0,0 +1,14 @@
+﻿
+$asm_path = 'C:\git\IISSharingAssemblies\common-assemblies-legacy'
+$gac_path = 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\gacutil.exe'
+
+#install assembly to GAC
+Get-ChildItem $asm_path | ForEach-Object {
+    & $gac_path "/i", $_.FullName
+}
+
+#uninstall assembly from GAC
+Get-ChildItem $asm_path | ForEach-Object {
+   & $gac_path "/u", ([System.Reflection.AssemblyName]::GetAssemblyName($_.FullName).FullName)
+}
+
---
+++ powershell-scripts/install-to-native-images-cache.ps1
@@ -0,0 +1,14 @@
+$asm_path = 'C:\git\IISSharingAssemblies\common-assemblies-legacy'
+$ngn_path = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe'
+
+#install native images from cache
+Get-ChildItem $asm_path | ForEach-Object {
+   & $ngn_path "install", ([System.Reflection.AssemblyName]::GetAssemblyName($_.FullName).FullName)
+}
+
+
+#uninstall native images from cache
+Get-ChildItem $asm_path | ForEach-Object {
+   & $ngn_path "uninstall", ([System.Reflection.AssemblyName]::GetAssemblyName($_.FullName).FullName)
+}
+
