a6ae31afe1fca9542793a2c7646001c2781c6c44
Stanislav Flusov <Stanislav.Flusov@mysale.com>
2017-09-20 13:49:38 +0300

    install unsigned assemblies to cache

---
+++ powershell-scripts/install-unsigned-assemblies-to-cache.ps1
@@ -0,0 +1,14 @@
+﻿$asm_path = 'C:\ASPNETCommonAssemblies'
+$ngn_path = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe'
+
+#install native images from cache
+Get-ChildItem -recurse $asm_path | ForEach-Object {
+   Write-Host "Try to install assemblies by path $_"
+   & $ngn_path "install", $_.FullName
+}
+
+#unintall native images from cache
+#Get-ChildItem -recurse $asm_path | ForEach-Object {
+#   Write-Host "Try to install assemblies by path $_"
+#   & $ngn_path "uninstall", $_.FullName
+#}
