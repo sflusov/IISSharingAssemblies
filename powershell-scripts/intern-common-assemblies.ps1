0612bb372474e9f59515147e27992df974053277
Stanislav Flusov <Stanislav.Flusov@mysale.com>
2017-09-20 13:37:40 +0300

    add powershell script intern-common-assemblies

---
+++ powershell-scripts/intern-common-assemblies.ps1
@@ -0,0 +1,8 @@
+﻿#Windows 10 path
+$intern_path = 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\aspnet_intern.exe'
+
+$intern_param = '-mode', 'exec', 
+                '-sourcedir', 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files',
+                '-interndir', 'C:\ASPNETCommonAssemblies'
+
+& $intern_path $intern_param
